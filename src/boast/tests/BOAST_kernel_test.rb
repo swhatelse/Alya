b195f81bc6753b47dda30943d075a45d66275629
swhatelse <stevenwhatelse@hotmail.fr>
2016-11-04 15:06:05 +0100

    5th nest + corrected 4th nest + complete check

--- src/boast/tests/BOAST_kernel_test.rb
+++ src/boast/tests/BOAST_kernel_test.rb
@@ -8,13 +8,10 @@ class TestFor < Minitest::Test
   def init(vector_size,dim,seed)
     ANArray.srand(seed) if seed
 
-
-    @kfl_lumped = 1
     @mnode = 10
     @ntens = 2
     @kfl_duatss = 1
     @fact_duatss = 2
-    @kfl_stabi_nsi = 1 # or -1
     @fvins_nsi = 2.0
     @fcons_nsi = 0.5
     @bemol_nsi = 1.0
@@ -29,7 +26,6 @@ class TestFor < Minitest::Test
     @nbdfp_nsi = 5
     @kfl_sgsti_nsi = 1
     @kfl_nota1_nsi = 0
-    @kfl_limit_nsi = 1 # or 2
     @kfl_penal_nsi = 1
     @penal_nsi = 1.0
     @kfl_bubbl_nsi = 1
@@ -103,8 +99,10 @@ class TestFor < Minitest::Test
 
   end
   def test_nests_orig_vs_boast
+    epsilon = 10e-15
     set_fortran_line_length(100)
-    nests = [1,2,3]
+    nests = [1,2,3,4,5]
+
     (1..2).each{|vector_size|
       k_orig_params = {:vector_length => vector_size, :preprocessor => false, :nests => nests}
       k_boast_params = {:vector_length => vector_size, :nests => nests}
@@ -114,12 +112,22 @@ class TestFor < Minitest::Test
 
       (2..3).each{|dim|
         init(vector_size,dim,10)
-        [FORTRAN].each{|lang|
-          set_lang(lang)
-          k_boast = generate_boast_implem(k_boast_params)
-          k_boast.build
 
-          k_orig.run(@pnode,@pgaus,@gpden,@gpvis,@gppor,@gpsp1,@gpsp2,@gpvol,@gpsha,@gpcar,@gpadv,
+        (1..2).each{|kfl_lumped|
+          next if kfl_lumped == 1 and dim == 2
+          @kfl_lumped = kfl_lumped
+
+          [-1,1].each{|kfl_stabi_nsi|
+            @kfl_stabi_nsi = kfl_stabi_nsi
+
+            (1..2).each{|kfl_limit_nsi|
+              @kfl_limit_nsi = kfl_limit_nsi
+              [FORTRAN].each{|lang|
+                set_lang(lang)
+                k_boast = generate_boast_implem(k_boast_params)
+                k_boast.build
+
+                k_orig.run(@pnode,@pgaus,@gpden,@gpvis,@gppor,@gpsp1,@gpsp2,@gpvol,@gpsha,@gpcar,@gpadv,
                            @gpvep,@gpgrp,@gprhs,@gprhc,@gpvel,@gpsgs,@elvel,@elpre,@elbub,@elauu_ref,@elaup_ref,
                            @elapp_ref,@elapu_ref,@elrbu_ref,@elrbp_ref,@dtinv_loc,@dtsgs,@pbubl,@gpsha_bub,@gpcar_bub,
                            @elauq_ref,@elapq_ref,@elaqu_ref,@elaqp_ref,@elaqq_ref,@elrbq_ref,
@@ -130,26 +138,29 @@ class TestFor < Minitest::Test
                            @kfl_sgsti_nsi,@kfl_nota1_nsi,@kfl_limit_nsi,@kfl_penal_nsi,@penal_nsi,
                            @kfl_bubbl_nsi,@ndime,@agrau_ref,@wgrgr_ref)
 
-          k_boast.run(@pnode,@pgaus,@gpden,@gpvis,@gppor,@gpsp1,@gpsp2,@gpvol,@gpsha,@gpcar,@gpadv,
-                           @gpvep,@gpgrp,@gprhs,@gprhc,@gpvel,@gpsgs,@elvel,@elpre,@elbub,@elauu_boast,@elaup_boast,
-                           @elapp_boast,@elapu_boast,@elrbu_boast,@elrbp_boast,@dtinv_loc,@dtsgs,@pbubl,@gpsha_bub,@gpcar_bub,
-                           @elauq_boast,@elapq_boast,@elaqu_boast,@elaqp_boast,@elaqq_boast,@elrbq_boast,
-                           # Original global variables
-                           @kfl_lumped,@mnode,@ntens,@kfl_duatss,@fact_duatss,@kfl_stabi_nsi,@fvins_nsi,
-                           @fcons_nsi,@bemol_nsi,@kfl_regim_nsi,@fvela_nsi,@kfl_rmom2_nsi,@kfl_press_nsi,
-                           @kfl_p1ve2_nsi,@kfl_linea_nsi,@kfl_confi_nsi,@nbdfp_nsi,
-                           @kfl_sgsti_nsi,@kfl_nota1_nsi,@kfl_limit_nsi,@kfl_penal_nsi,@penal_nsi,
-                           @kfl_bubbl_nsi,@ndime,@agrau_boast,@wgrgr_boast)
+                k_boast.run(@pnode,@pgaus,@gpden,@gpvis,@gppor,@gpsp1,@gpsp2,@gpvol,@gpsha,@gpcar,@gpadv,
+                            @gpvep,@gpgrp,@gprhs,@gprhc,@gpvel,@gpsgs,@elvel,@elpre,@elbub,@elauu_boast,@elaup_boast,
+                            @elapp_boast,@elapu_boast,@elrbu_boast,@elrbp_boast,@dtinv_loc,@dtsgs,@pbubl,@gpsha_bub,@gpcar_bub,
+                            @elauq_boast,@elapq_boast,@elaqu_boast,@elaqp_boast,@elaqq_boast,@elrbq_boast,
+                            # Original global variables
+                            @kfl_lumped,@mnode,@ntens,@kfl_duatss,@fact_duatss,@kfl_stabi_nsi,@fvins_nsi,
+                            @fcons_nsi,@bemol_nsi,@kfl_regim_nsi,@fvela_nsi,@kfl_rmom2_nsi,@kfl_press_nsi,
+                            @kfl_p1ve2_nsi,@kfl_linea_nsi,@kfl_confi_nsi,@nbdfp_nsi,
+                            @kfl_sgsti_nsi,@kfl_nota1_nsi,@kfl_limit_nsi,@kfl_penal_nsi,@penal_nsi,
+                            @kfl_bubbl_nsi,@ndime,@agrau_boast,@wgrgr_boast)
 
-          diff_agrau = (@agrau_ref - @agrau_boast).abs
-          diff_wgrgr = (@wgrgr_ref - @wgrgr_boast).abs
-          diff_elauu = (@elauu_ref - @elauu_boast).abs
-          diff_elrbu = (@elrbu_ref - @elrbu_boast).abs
-          
-          assert((diff_agrau > epsilon).to_a.flatten.include? 1)
-          assert((diff_wgrgr > epsilon).to_a.flatten.include? 1)
-          assert((diff_elauu > epsilon).to_a.flatten.include? 1)
-          assert((diff_elrbu > epsilon).to_a.flatten.include? 1)
+                diff_agrau = (@agrau_ref - @agrau_boast).abs
+                diff_wgrgr = (@wgrgr_ref - @wgrgr_boast).abs
+                diff_elauu = (@elauu_ref - @elauu_boast).abs
+                diff_elrbu = (@elrbu_ref - @elrbu_boast).abs
+                
+                assert( (not (diff_agrau > epsilon).to_a.flatten.include? 1))
+                assert( (not (diff_wgrgr > epsilon).to_a.flatten.include? 1))
+                assert( (not (diff_elauu > epsilon).to_a.flatten.include? 1))
+                assert( (not (diff_elrbu > epsilon).to_a.flatten.include? 1))
+              }
+            }
+          }
         }
       }
     }
